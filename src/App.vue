88cfb4ded23ae2813f936f43500483f067026ae3
Tyler Howard <thowar4@students.towson.edu>
2018-01-19 21:21:07 -0500

    Initial commit with start of implementation

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <div class = "content">
+      <h1>Markup Editor</h1>
+      <div class = "text-editor">
+        <TextEditor/>
+      </div>
+    </div>
+    <footer class = "footer">
+       <p>Bootstrapped quickly with Vue</p>
+       <img src="./assets/logo.png"/>
+       <small>Using ToastUIEditor</small>
+    </footer>
+  </div>
+</template>
+
+<script>
+import TextEditor from './components/TextEditor'
+
+export default {
+  name: 'App',
+  components: {
+    TextEditor
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding:0px;
+  margin: 0px;
+}
+html {
+  height: 100%;
+}
+.content {
+  flex: 1 0 auto;
+}
+.footer {
+  border-top: 1px solid lightgrey;
+  flex-shrink: 0;
+  padding-top:10px;
+  padding-bottom:10px;
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  align-items: center;
+  height:auto;
+  width:100%;
+  background-color: whitesmoke;
+}
+h1 {
+  background-color: whitesmoke;
+  padding:40px;
+  text-align: center;
+  border-bottom: 1px solid lightgrey;
+}
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: white;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.text-editor{
+  margin:50px;
+  height: 100%;
+}
+footer img {
+  height:50px;
+  width:50px;
+}
+</style>
---
+++ src/components/TextEditor.vue
@@ -0,0 +1,32 @@
+<template>
+    <tui-editor :options="options"/>
+</template>
+
+<script>
+export default {
+  name: 'TextEditor',
+  data () {
+    return {
+      options : {
+            initialValue: `# Project Title
+One Paragraph of project description goes here
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+### Prerequisites
+What things you need to install the software and how to install them
+\`\`\`
+git clone REPO_URL
+\`\`\`
+            `,
+            initialEditType: 'markdown',
+            previewStyle: 'vertical',
+            height: 'auto',
+            minHeight: '500px'
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>

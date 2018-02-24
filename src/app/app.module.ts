c23d4e569601e755e2a11aaeaacb75f8bd78c1b5
Tyler Howard <thowar4@students.towson.edu>
2018-02-24 13:19:10 -0500

    Add default route for guard and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,34 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {TuiModule} from 'ngx-tui-editor';
-
+import {PopoverModule} from 'ngx-popover';
 import { AppComponent } from './app.component';
 import { TextEditorComponent } from './text-editor/text-editor.component';
+import { HeaderComponent } from './header/header.component';
+import { ControlButtonsComponent } from './control-buttons/control-buttons.component';
+import { FormsModule } from '@angular/forms';
+import { ValueUpdaterService } from './services/value-updater.service';
+import { PendingChangesGuard } from './services/pending-changes.guard';
 
-
+export const routes: Routes = [
+  { path: '', component: TextEditorComponent, canDeactivate: [PendingChangesGuard] },
+];
 @NgModule({
   declarations: [
     AppComponent,
-    TextEditorComponent
+    TextEditorComponent,
+    HeaderComponent,
+    ControlButtonsComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    TuiModule
+    FormsModule,
+    TuiModule,
+    PopoverModule,
   ],
-  providers: [],
+  providers: [ValueUpdaterService,PendingChangesGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

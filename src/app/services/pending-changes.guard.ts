1dbef832f22956daa18e70781c3e23924111762c
Tyler Howard <thowar4@students.towson.edu>
2018-02-24 13:19:54 -0500

    Add guard for pending changes to popup window

---
+++ src/app/services/pending-changes.guard.ts
@@ -0,0 +1,11 @@
+import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+export interface ComponentCanDeactivate {canDeactivate: () => boolean | Observable<boolean>;}
+
+export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
+    canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
+        return component.canDeactivate() ?
+            true : confirm('WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.');
+    }
+}

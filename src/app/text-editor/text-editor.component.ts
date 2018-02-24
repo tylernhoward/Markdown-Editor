de2a2cfd1e282023b68cc84f8dce212770b3c2b3
Tyler Howard <thowar4@students.towson.edu>
2018-02-24 13:16:41 -0500

    Allow file name to be changed

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,36 +1,41 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { TuiService } from 'ngx-tui-editor';
+import { ValueUpdaterService } from './../services/value-updater.service';
+import { Component, OnInit, ElementRef, ChangeDetectorRef, Input, Output, EventEmitter, HostListener } from '@angular/core';
+import { ComponentCanDeactivate } from './../services/pending-changes.guard';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-text-editor',
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.css']
 })
-export class TextEditorComponent implements OnInit {
-  @ViewChild('downloader') downloader:ElementRef
+export class TextEditorComponent implements OnInit, ComponentCanDeactivate {
   options: object
-  textURI = null;
-  constructor(private editorService:TuiService) { }
-
+  el:HTMLElement
+  fileName:string;
+  constructor(private cdr: ChangeDetectorRef, private editorService: TuiService, private valueUpdater: ValueUpdaterService) {}
   ngOnInit() {
+    this.fileName = "Readme"
     this.options = {
             initialValue: `# Title of Project` ,
             initialEditType: 'markdown',
             previewStyle: 'vertical',
             height: 'auto',
-            minHeight: '500px'
+            minHeight: "100%"
           }
   }
-  async save(){
-    await this.createFile();
 
+  @HostListener('window:beforeunload')
+  canDeactivate(): Observable<boolean> | boolean {
+    if(this.editorService.getMarkdown() === this.options["initialValue"]){
+      return true;
+    }
+    else{
+      return false;
+
+    }
   }
-  createFile(){
-    let textBlob = new Blob([this.editorService.getMarkdown()], { type: 'text/plain' });
-    this.textURI = URL.createObjectURL(textBlob);
-    this.downloader.nativeElement.href = this.textURI;
-    this.downloader.nativeElement.click()
-
+  updateFileName(event) {
+    this.valueUpdater.emit(this.fileName);
   }
-
 }

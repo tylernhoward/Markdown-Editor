51a301faeb2d013e8044ec9d45afc9e22101bd1a
Tyler Howard <thowar4@students.towson.edu>
2018-02-24 13:17:31 -0500

    Add control buttons to handle operations

---
+++ src/app/control-buttons/control-buttons.component.ts
@@ -0,0 +1,63 @@
+import { ValueUpdaterService } from './../services/value-updater.service';
+import { TuiService } from 'ngx-tui-editor';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-control-buttons',
+  templateUrl: './control-buttons.component.html',
+  styleUrls: ['./control-buttons.component.css']
+})
+export class ControlButtonsComponent implements OnInit {
+  @ViewChild('downloader') downloader: ElementRef;
+  @ViewChild('previewer') previewer: ElementRef
+  textURI = null;
+  fileName: string;
+  filePath:string;
+  constructor(private editorService: TuiService, private valueUpdater: ValueUpdaterService) { }
+
+  ngOnInit() {
+    this.fileName = "Readme";
+    this.valueUpdater.nameObservable.subscribe(value => {
+      this.fileName = value;
+    });
+  }
+  openSettings() {
+
+  }
+
+  save(type) {
+    this.addExtension(type)
+    this.createFile(type);
+    this.downloadFile();
+  }
+  preview(type) {
+    this.createFile(type);
+    this.previewFile();
+  }
+
+  addExtension(type){
+    this.filePath = "";
+    switch(type){
+      case "html": this.filePath = this.fileName + ".html";break;
+      case "markdown": this.filePath = this.fileName + ".md"; break;
+      default: this.filePath = this.fileName + ".md";
+    }
+  }
+
+  private createFile(type) {
+    let content = (type === "markdown" ? this.editorService.getMarkdown(): this.editorService.getHtml());
+    let textBlob = new Blob([content], { type: 'text/plain' });
+    this.textURI = URL.createObjectURL(textBlob);
+  }
+  private downloadFile(){
+    this.downloader.nativeElement.href = this.textURI;
+    this.downloader.nativeElement.download = this.filePath;
+    this.downloader.nativeElement.click();
+  }
+  private previewFile() {
+    this.previewer.nativeElement.href = this.textURI;
+    this.previewer.nativeElement.click();
+  }
+
+
+}
